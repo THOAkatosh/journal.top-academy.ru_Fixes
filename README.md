59e8e02dd1c359bfb11c193c3a33f90a7095d8e0
THOAkatosh <vlad555335@gmail.com>
2024-10-01 00:15:08 +0300

    Removed unnecessary shit, added required font, edited readme
    Эх шарик, я как и ты был на цепи...

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # journal.top-academy_Fixes
 Fixes various dog decisions made by devs of this shit ass site.
 
-Personally, I use  [`User JavaScript and CSS`](https://chromewebstore.google.com/detail/user-javascript-and-css/nbhcbdghjpllgmfilhnhkllmkecfmpld) to upload [`styles.css`](styles.css) and [`scripts.js`](scripts.js) to site. But you can use [`Stylus`](https://chromewebstore.google.com/detail/stylus/clngdbkpkpeebahjckkjfobafhncgmne) with [`Tampermonkey`](https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) or whatever you want, it should work.
+I use  [`User JavaScript and CSS`](https://chromewebstore.google.com/detail/user-javascript-and-css/nbhcbdghjpllgmfilhnhkllmkecfmpld) to upload [`styles.css`](styles.css) and [`scripts.js`](scripts.js) to site. You can use whatever you want, but work is not guaranteed.
+
+**!!! IMPORTANT !!!**<br/>
+You **must** enable `Run on start` in [`User JavaScript and CSS`](https://chromewebstore.google.com/detail/user-javascript-and-css/nbhcbdghjpllgmfilhnhkllmkecfmpld) to make everything work: <br/>
+![runOnStart](https://i.imgur.com/ryBqP2r.png)
+
+There is a custom full name font, requiring [`minecraft_rus.ttf`](minecraft_rus.ttf): <br/>
+![fullName](https://i.imgur.com/De2PDHZ.png)
+
+If you don't like it or don't want to download another file just change the value of `--aka-text-font` in `:root` at the beginning of [`styles.css`](styles.css).
 
 ![dolbit](https://i.imgur.com/olmHX1r.gif)
 
--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
+// adding info to the page name depending on its content
 function nameFix() {
-    var win = window.location.href
+    var win = window.location.href;
     var dick = {
         "dashboard": "Главная",
         "schedule": "Расписание",
@@ -15,81 +16,90 @@ function nameFix() {
         "contacts": "Администрация",
         "signal": "Обращения",
         "market": "Магазин",
-    }
+    };
     for (var key in dick) {
         if (win.includes(key)) {
-            //document.title = "Journal - " + dick[key]
-            document.title = "DogKal - " + dick[key]
+            document.title = "Journal - " + dick[key]
+            //document.title = "DogKal - " + dick[key];
         }
     }
 }
-setInterval(nameFix, 100);
+setInterval(nameFix, 300);
 
-function doFunny() {
-    var barabama = [];
-    var doglet = document.querySelector("body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(4) > div");
-    //console.log(doglet);
-    setTimeout(function(){
-        var sons = doglet.querySelectorAll(".homework-item");
-        sons.forEach(element => {
-            let data = element.innerText.split("\n")[4].split(".")
-            barabama.push({date: new Date(`${data[2]}-${data[1]}-${data[0]}`), selector: element});
-        });
-        barabama.sort((a, b) => a.date - b.date);
-        let x = 0;
-        barabama.forEach(element => {
-            element.selector.style.order = x;
-            x++;
-        });
-        //console.log(barabama)
-    }, 500);
-    console.log("doFunny() - Sorting homework...")
-};
-
-function hwFix() {
-    if (document.URL == "https://journal.top-academy.ru/ru/main/homework/page/index") {
-        setTimeout(function(){
-            var dogs = [
-                ["0", "body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(1)"],
-                ["3", "body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(2)"],
-                ["2", "body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(3)"],
-                ["1", "body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(4)"]
-            ];
-            dogs.forEach(element => { document.querySelector(element[1]).style.order = element[0] });
-        }, 1500);
-        setTimeout(doFunny, 1500);
-        setTimeout(function(){
-            var nawRow = document.querySelector("body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.homeworks-navigation.row")
-            nawRow.style.justifyContent = "flex-end";
-            var updateButt = document.createElement("button")
-            updateButt.innerHTML = "Отсортировать ДЗ"
-            Object.assign(updateButt.style, {
-                position: "relative",
-                right: "15px",
-                margin: "20px 0 -60px 0",
-                width: "200px",
-                height: "49.51px",
-                backgroundColor: "white",
-                borderRadius: "25px",
-                outline: "0",
-                border: "1px solid #e5ebf2",
-            });
-            nawRow.appendChild(updateButt);
-            updateButt.addEventListener('mouseover', () => {
-                updateButt.style.filter = 'opacity(80%)';
-                updateButt.style.cursor = "pointer";
-            });
-            updateButt.addEventListener('mouseout', () => {
-                updateButt.style.filter = 'opacity(100%)';
-                updateButt.style.cursor = "unset";
-            });
-            updateButt.addEventListener("click", doFunny);
-            console.log("hwFix() - Loaded...")
-        }, 1500);
-    } else {
-        console.log("hwFix() - Not a homework page, not starting...")
+// revealing all current homework by pressing "show more"
+function revealHW() {
+    if (document.URL == "https://journal.top-academy.ru/ru/main/homework/page/index" && document.body.contains(document.querySelector("body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(4)"))) {
+        document.querySelector("body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(4)").querySelector("body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(4) > p.text-center > span").click();
     }
 }
-window.navigation.addEventListener("navigate", () => {
-    hwFix();
-});
+setInterval(revealHW, 500);
+
+// sort by exp. date function, then call a function
+function sortByDate() {
+	if (document.URL == "https://journal.top-academy.ru/ru/main/homework/page/index") {
+	    var barabama = [];
+	    var doglet = document.querySelector("body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(4) > div");
+	    var sons = doglet.querySelectorAll(".homework-item");
+	    sons.forEach(element => {
+	        let data = element.innerText.split("\n")[4].split(".");
+	        barabama.push({date: new Date(`${data[2]}-${data[1]}-${data[0]}`), selector: element});
+	    });
+	    barabama.sort((a, b) => a.date - b.date);
+	    let x = 0;
+	    barabama.forEach(element => {
+	        element.selector.style.order = x;
+	        x++;
+	    });
+	}
+} 
+setInterval(sortByDate, 1500);
+
+// function hwFix() {
+//     if (document.URL == "https://journal.top-academy.ru/ru/main/homework/page/index") {
+//     	setInterval(sortByDate, 2500);
+    	
+//     	//done in styles.css
+//         setTimeout(function(){
+//             var dogs = [
+//                 ["0", "body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(1)"],
+//                 ["3", "body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(2)"],
+//                 ["2", "body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(3)"],
+//                 ["1", "body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(4)"]
+//             ];
+//             dogs.forEach(element => { document.querySelector(element[1]).style.order = element[0] });
+//         }, 1500);
+        
+//         // useless sort button
+//         setTimeout(function(){
+//             var nawRow = document.querySelector("body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.homeworks-navigation.row")
+//             nawRow.style.justifyContent = "flex-end";
+//             var updateButt = document.createElement("button")
+//             updateButt.innerHTML = "Отсортировать ДЗ"
+//             Object.assign(updateButt.style, {
+//                 position: "relative",
+//                 right: "15px",
+//                 margin: "20px 0 -60px 0",
+//                 width: "200px",
+//                 height: "49.51px",
+//                 backgroundColor: "white",
+//                 borderRadius: "25px",
+//                 outline: "0",
+//                 border: "1px solid #e5ebf2",
+//             });
+//             nawRow.appendChild(updateButt);
+//             updateButt.addEventListener('mouseover', () => {
+//                 updateButt.style.filter = 'opacity(80%)';
+//                 updateButt.style.cursor = "pointer";
+//             });
+//             updateButt.addEventListener('mouseout', () => {
+//                 updateButt.style.filter = 'opacity(100%)';
+//                 updateButt.style.cursor = "unset";
+//             });
+//             updateButt.addEventListener("click", sortByDate);
+//         }, 1500);
+//     }
+// }
+//
+// window.navigation.addEventListener("navigate", () => {
+//     hwFix();
+// });
