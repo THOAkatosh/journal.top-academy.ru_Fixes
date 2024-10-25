3d287c1d53bfc55ababaf908ec1ae46ce51259e6
THOAkatosh <vlad555335@gmail.com>
2024-10-25 23:01:15 +0300

    Added "Funnyes" button containing visual filters

--- scripts.js
+++ scripts.js
@@ -19,11 +19,157 @@ function nameFix() {
     };
     for (var key in dick) {
         if (win.includes(key)) {
-            document.title = "Journal - " + dick[key]
+            document.title = "Journal - " + dick[key];
             //document.title = "DogKal - " + dick[key];
         }
     }
 }
+
+// funnyes button
+function buttonCreate() {
+	const container = document.createElement("div");
+		container.style.position = "fixed";
+		container.style.bottom = "20px";
+		container.style.right = "20px";
+		container.style.zIndex = "3000";
+	 
+	const toggleButton = document.createElement("button");
+		toggleButton.textContent = "Funnyes";
+		toggleButton.style.padding = "10px 10px";
+		toggleButton.style.fontSize = "16px";
+		toggleButton.style.cursor = "pointer";
+		toggleButton.style.borderRadius = "8px";
+		toggleButton.style.border = "unset";
+		toggleButton.style.outline = "unset";
+		toggleButton.style.fontWeight = "bolder";
+		toggleButton.style.transitionDuration = "0.4s";
+		toggleButton.style.color = "#fff";
+		toggleButton.style.letterSpacing = "2.5px";
+		toggleButton.style.backgroundColor = "#00000075"; 
+		toggleButton.style.boxShadow = "#00000075 0px 0px 1px 1px, inset #00000075 0px 0px 2px 2px";
+		toggleButton.style.textShadow = "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000";
+		toggleButton.onmouseover = () => {
+			toggleButton.style.boxShadow = "#00000045 0px 0px 2px 2px, inset #00000025 0px 0px 2px 2px";
+			toggleButton.style.backgroundColor = "#00000090";
+			toggleButton.style.letterSpacing = "3px";
+		};
+		toggleButton.onmouseout = () => {
+			toggleButton.style.boxShadow = "#00000075 0px 0px 1px 1px, inset #00000075 0px 0px 2px 2px";
+			toggleButton.style.backgroundColor = "#00000075";
+			toggleButton.style.letterSpacing = "2.5px";
+		};
+		
+	const menu = document.createElement("div");
+		menu.style.display = "none";
+		menu.style.position = "absolute";
+		menu.style.bottom = "50px";
+		menu.style.right = "0";
+		menu.style.backgroundColor = "#00000075";
+		menu.style.color = "#fff";
+		menu.style.borderRadius = "8px";
+		menu.style.boxShadow = "0 4px 8px #00000020";
+		menu.style.width = "fit-content";
+		menu.style.boxShadow = "#00000075 0px 0px 1px 1px, inset #00000075 0px 0px 2px 2px";
+	
+	const items = ["LGTV", "SadMode", /*"Invert",*/ "PoorEyesight"];
+	items.forEach((text) => {
+		const menuItem = document.createElement("div");
+		menuItem.className = text;
+		menuItem.textContent = text;
+		menuItem.style.userSelect = "none";
+		menuItem.style.padding = "10px";
+		menuItem.style.cursor = "pointer";
+		menuItem.style.backgroundColor = "#ffffff00";
+		menuItem.style.letterSpacing = "0.5px";
+		menuItem.style.borderRadius = "8px";
+		menuItem.style.fontWeight = "bolder";
+		menuItem.style.color = "red"; 
+		menuItem.style.textShadow = "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000";
+		menuItem.onmouseover = () => (menuItem.style.backgroundColor = "#00000030");
+		menuItem.onmouseout = () => (menuItem.style.backgroundColor = "#ffffff00");
+		menu.appendChild(menuItem);
+	});
+	
+	toggleButton.addEventListener("click", () => {
+		menu.style.display = menu.style.display === "none" ? "block" : "none";
+	});
+	
+	container.appendChild(toggleButton);
+	container.appendChild(menu);
+	document.body.appendChild(container);
+	
+	const LGTV_button = document.querySelector(".LGTV");
+	const SadMode_button = document.querySelector(".SadMode");
+	// const Invert_button = document.querySelector(".Invert");
+	const PoorEyesight_button = document.querySelector(".PoorEyesight");
+	
+	let LGTV_status = false;
+	let SadMode_status = false;
+	// let Invert_status = false;
+	let PoorEyesight_status = false;
+
+	const bodyElements = document.querySelectorAll("*:not(body)");
+	
+	setInterval(() => {
+	    bodyElements.forEach(element => {
+	        let filter = '';
+	
+	        if (LGTV_status) {
+	            const LG_color = (Date.now() / 50) % 360;
+	            filter += `hue-rotate(${LG_color}deg) `;
+	        }
+	        if (SadMode_status) {
+	            filter += `grayscale(0.65)`;
+	        }
+	        // if (Invert_status) {
+	        //     filter += `invert(1)`;
+	        // }
+	        if (PoorEyesight_status) {
+	            filter += `blur(3px)`;
+	        }
+	        
+	        element.style.filter = filter.trim() || "unset";
+	    });
+	}, 100);
+	
+	if (LGTV_button) {
+	    LGTV_button.addEventListener("click", () => {
+	        LGTV_status = !LGTV_status;
+	        LGTV_button.style.color = LGTV_status ? "green" : "red";
+	        LGTV_button.style.textShadow = LGTV_status ? 
+	        			"-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff" : 
+	        			"-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000";
+	    });
+	}
+	if (SadMode_button) {
+	    SadMode_button.addEventListener("click", () => {
+	        SadMode_status = !SadMode_status;
+	        SadMode_button.style.color = SadMode_status ? "green" : "red";
+	        SadMode_button.style.textShadow = SadMode_status ? 
+	        			"-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff" : 
+	        			"-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000";
+	    });
+	}
+	// if (Invert_button) {
+	//     Invert_button.addEventListener("click", () => {
+	//         Invert_status = !Invert_status;
+	//         Invert_button.style.color = Invert_status ? "green" : "red";
+	// 		Invert_button.style.textShadow = Invert_status ? 
+	// 					"-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff" : 
+	// 					"-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000";
+	//     });
+	// }
+	if (PoorEyesight_button) {
+	    PoorEyesight_button.addEventListener("click", () => {
+	        PoorEyesight_status = !PoorEyesight_status;
+	        PoorEyesight_button.style.color = PoorEyesight_status ? "green" : "red";
+	        PoorEyesight_button.style.textShadow = PoorEyesight_status ? 
+	        			"-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff" : 
+	        			"-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000";
+	    });
+	}
+}
+setTimeout(buttonCreate, 500);
 
 // revealing all current homework by pressing "show more"
 function revealHW() {
@@ -43,7 +189,7 @@ function sortByDate() {
 	    var doglet = document.querySelector("body > mystat > ng-component > ng-component > div > div.content > div.wrapper > ng-component > div > div.items-homework > hw-item:nth-child(4) > div");
         let sons = doglet.querySelectorAll(".homework-item");
 	    sons.forEach(son => {
-            let dogs = son.querySelectorAll(".item-footer")
+            let dogs = son.querySelectorAll(".item-footer");
             dogs.forEach(element => {
                 let data = element.innerText.split("\n")[2].split(".");
                 barabama.push({date: new Date(`${data[2]}-${data[1]}-${data[0]}`), selector: son});
